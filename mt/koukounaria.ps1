0f8a289e90a0083c17af644f598bad37ec700ad4
Ioannis Kokkinis <info@upg.gr>
2025-08-20 21:01:59 +0300

    F

--- mt/koukounaria.ps1
+++ mt/koukounaria.ps1
@@ -3,8 +3,8 @@ Import-Module Posh-SSH -ErrorAction Stop
 
 # ======== USER CONFIG ========
 $BaseNet        = "192.168.208."
-$StartIP        = 2          # inclusive
-$EndIP          = 253         # inclusive
+$StartIP        = 138          # inclusive
+$EndIP          = 141         # inclusive
 $Username       = "admin"
 $Password       = "is3rupgr.1821##"   # will be set if device has no password
 $SSID           = "Koukounaria Guest 9"
